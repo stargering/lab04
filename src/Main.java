e28ea8817e37fa3f35a86cc0a6bc971bf2f292f1
kartik <kartik.kushwaha.3011@gmail.com>
2024-05-04 15:25:12 +0200

    I have added a readChar method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        getNextCharacter("/Users/khushirajput/Desktop/Info Übung/Lab-4/lab04/src/colors.txt");
     }
+
+    public static char getNextCharacter(String filePath) {
+        int nextChar = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while ((nextChar = reader.read()) != -1) {
+               
+                if (nextChar == '\r') {
+                    continue;
+                }
+                System.out.print((char) nextChar);
+            }
+        } catch (IOException e) {
+            System.out.println("There is an error in reading file");
+            e.printStackTrace();
+        }
+        return (char)nextChar;
+    }
+
+
 }
